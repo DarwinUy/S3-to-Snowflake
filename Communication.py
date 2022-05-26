54ed584f4d0d1f12bf8b983e3e4133fbc19ec34b
DarwinUy <50888888+DarwinUy@users.noreply.github.com>
2022-05-25 17:01:41 -0700

    Modules
    These Modules were created to be able to use common function in various programs as well as clean code

---
+++ Communication.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# title           :Communications.py
+# description     :This is where we store the email function
+# author          :Darwin Uy
+# date            :2021-11-01
+# version         :0.1
+# usage           :python pyscript.py
+# notes           :
+# python_version  :3.9
+# ==============================================================================
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib, ssl
+
+
+# Email
+def send_mail(sender_email, receiver_email, subject, body, attachments=[]):
+    def function():
+        ''' Description: This is a function that
+            Input:
+                sender_email
+                receiver_email: List of strings that are the reciever email addresses
+                subject: String that will be the email subject
+                body
+                attachments
+
+            Output:
+
+            '''
+        {}
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = ', '.join(receiver_email)
+    message["Subject"] = subject
+
+    #    message["Bcc"] = receiver_email  # Recommended for mass emails
+    # Add body to email
+    message.attach(MIMEText(body, "plain"))
+    filename = attachments
+    # In same directory as script
+    # Open files in binary mode
+    for file in filename:
+        with open(file, "rb") as attachment:
+            # Add file as application/octet-stream
+            # Email client can usually download this automatically as attachment
+            mbase = MIMEBase("application", "octet-stream")
+            mbase.set_payload(attachment.read())
+        # Encode file in ASCII characters to send by email
+        encoders.encode_base64(mbase)
+        # Add header as key/value pair to attachment part
+        mbase.add_header(
+            "Content-Disposition",
+            f"attachment; filename= {file}",
+        )
+
+        # Add attachment to message and convert message to string
+        message.attach(mbase)
+    text = message.as_string()
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP("smtp.ascap.com") as server:
+        server.sendmail(sender_email, receiver_email, text)
+    print("Mail sent to user")
