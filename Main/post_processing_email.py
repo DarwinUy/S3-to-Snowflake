7ad0df286a3e762d5f080c2fea0395f4e06d4d03
DarwinUy <50888888+DarwinUy@users.noreply.github.com>
2022-06-01 18:57:17 -0700

    Update post_processing_email.py

--- Main/post_processing_email.py
+++ Main/post_processing_email.py
@@ -1,17 +1,17 @@
 import sys
 import yaml
 import pandas
-sys.path.insert(0, '/home/ec2-user/production/S3_to_Snowflake/source')
+sys.path.insert(0, '')
 
 import Communication
 import S3Connection
 import SnowflakeConnection
 import PandasProcessing
 
-with open(r'/home/ec2-user/production/S3_to_Snowflake/config/S3_Configurations.yaml') as file:
+with open(r'') as file:
     s3Config = yaml.load(file, Loader=yaml.FullLoader)
 
-with open(r'/home/ec2-user/production/S3_to_Snowflake/config/Snowflake_Configurations.yaml') as file:
+with open(r'') as file:
     snowflakeConfig = yaml.load(file, Loader=yaml.FullLoader)
 
 # Get Credentials
@@ -31,12 +31,12 @@ files = []
 sfConn = SnowflakeConnection.createSnowflakeConnection(sfAccount = sfAccount, sfUser = sfUser, sfPrivateKey = sfPrivateKey, sfWarehouse = 'USERS_DATA', sfDatabase = 'INTERNATIONAL', sfSchema = 'PROCESSED')
 sfCur = sfConn.cursor()
 # Get processed CSV list
-sql = "SELECT DISTINCT FILENAME FROM INTERNATIONAL.PROCESSED.UP_CSV_OUTPUT_ARCHIVE order by FILENAME"
+sql = "SELECT DISTINCT FILENAME FROM TABLE order by FILENAME"
 sfCur.execute(sql)
 for record in sfCur:
     files.append(str(record[0]))
 # Get processed XLSX list
-sql = "SELECT DISTINCT FILENAME FROM INTERNATIONAL.PROCESSED.UP_XLSX_OUTPUT_ARCHIVE order by FILENAME"
+sql = "SELECT DISTINCT FILENAME FROM TABLE order by FILENAME"
 sfCur.execute(sql)
 for record in sfCur:
     files.append(str(record[0]))
